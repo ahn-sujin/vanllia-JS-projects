c4dc8ef0634b1955345385b5555f3683653e9ead
asj <asj9674@gmail.com>
2022-03-21 11:20:40 +0900

    로그인 버튼 활성화 두번째 방법

--- js/common.js
+++ js/common.js
@@ -1,31 +1,53 @@
+
+const formWrap = document.querySelector('.form_wrap_line');
+const idForm = document.querySelector('#user_id');
+const pwForm = document.querySelector('#user_pw');
+const loginButton = document.querySelector('#btn_login');
+
 // ========================
 // 1. 첫번째 방법
 // - 코드가 길어진다.
 // - 한번 활성화된 버튼이 다시 비활성화로 돌아가지 않는다.
 // ========================
+// let idCheck = false;
+// let pwCheck = false;
 
-const idForm = document.querySelector('#user_id');
-const passwordForm = document.querySelector('#user_pw');
-const loginButton = document.querySelector('#btn_login');
+// const pushValue = () => {
+//     idForm.addEventListener('keypress', () => {
+//         idCheck = true;
+//     });
 
-let checkid = false;
-let checkpw = false;
+//     pwForm.addEventListener('keypress', () => {
+//         pwCheck = true;
+//         if(idCheck && pwCheck){
+//             loginButton.disabled = false;
+//         }
+//     })
 
-const pushValue = () => {
-    idForm.addEventListener('keypress', ()=> {
-        checkid = true;
-    });
+//     loginButton.addEventListener('click', () => {
+//         if (!idForm.value || !pwForm.value){
+//             formWrap.classList.add('error');
+//         }
+//     })
+// }
 
-    passwordForm.addEventListener('keypress', ()=> {
-        checkpw = true;
-        if(checkid && checkpw){
-            loginButton.disabled = false; 
+// pushValue();
+
+// ========================
+// 2. 두번째 방법
+// - 비밀번호 입력 칸을 기준으로 입력이 되었을 때, 이벤트를 준다.
+// - 코드는 간단해 지지만, 활성화된 버튼이 다시 비활성화로 돌아가지 않는다.
+// ========================
+const pushValue = () =>{
+    pwForm.addEventListener('keypress' , () =>{
+        if(idForm.value && pwForm.value){
+            loginButton.disabled = false;
+        } else {
+            loginButton.disabled = true;
         }
-    });
-
+    })
 }
-pushValue();
-
+pushValue ();
 
 
 
