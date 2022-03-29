343e197bed813c2603a742c925c26204acb9fc06
asj <asj9674@gmail.com>
2022-03-29 17:27:51 +0900

    링크 브라우저 기본 이벤트 발생 막기

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
+const link = document.querySelector('a');
 
 function loginSubmit(event){
     const userName = loginInput.value;
     event.preventDefault();
     console.log(userName);
+    console.log(event);
+}
+function handleLinkClick(event){
+    event.preventDefault();
+    alert('welcome!')
+    console.log(event);
 }
 
 loginForm.addEventListener('submit', loginSubmit);
-
+link.addEventListener('click', handleLinkClick);
 
 
 
