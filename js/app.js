1126f57a8adbb9de8465005c61c93b7f20fb30a7
asj <asj9674@gmail.com>
2022-03-30 11:15:45 +0900

    loading username

--- js/app.js
+++ js/app.js
@@ -1,19 +1,33 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
 const greeting = document.querySelector('#greeting');
-const HIDDNE_CLASSNAME = 'hidden';
 
+const HIDDNE_CLASSNAME = 'hidden';
+const KEY_NAME = 'username'
 
 function loginSubmit(event){
     event.preventDefault();
     const userName = loginInput.value;
+    localStorage.setItem(KEY_NAME , userName);
+
     loginForm.classList.add(HIDDNE_CLASSNAME);
-    greeting.innerText = `Hello ${userName}!`;
-    greeting.classList.remove(HIDDNE_CLASSNAME);
+    paintGreeting(userName);
 }
 
+function paintGreeting(name) {
+    greeting.classList.remove(HIDDNE_CLASSNAME);
+    greeting.innerText = `Hello ${name}`;
+}
 
 loginForm.addEventListener('submit', loginSubmit);
+
+const userNameKey = localStorage.getItem(KEY_NAME); 
+
+if(userNameKey === null){
+    loginForm.classList.remove(HIDDNE_CLASSNAME);
+} else {
+    paintGreeting(userNameKey);
+}
 
 
 
