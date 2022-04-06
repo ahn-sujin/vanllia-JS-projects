55b2146d4dc5563a76ab7c2c073fbf18708ef7d7
asj <asj9674@gmail.com>
2022-04-06 15:58:44 +0900

    로그인 기능 구현중

--- js/greetings.js
+++ js/greetings.js
@@ -1,2 +1,14 @@
+const loginForm = document.querySelector('#login form');
+const loginInput = loginForm.querySelector('input');
+const loginButton = loginForm.querySelector('button');
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const userName = loginInput.value
+    console.log(userName);
+}
 
 
+
+loginForm.addEventListener('submit', onLoginSubmit)
+loginButton.addEventListener('click', onLoginSubmit)
