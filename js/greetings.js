c5e8dc1d49150dd00e564ecdeb1689e584cd57f4
asj <asj9674@gmail.com>
2022-04-19 15:48:25 +0900

    로그인 기능 구현 완료

--- js/greetings.js
+++ js/greetings.js
@@ -1,14 +1,40 @@
-const loginForm = document.querySelector('#login form');
+const loginForm = document.querySelector('#login');
 const loginInput = loginForm.querySelector('input');
 const loginButton = loginForm.querySelector('button');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASS = 'hidden';
+const LOCAL_STORAGE_KEY_NAME = 'username'
+
 
 function onLoginSubmit(event){
     event.preventDefault();
     const userName = loginInput.value
-    console.log(userName);
+    loginForm.classList.add(HIDDEN_CLASS);
+    localStorage.setItem(LOCAL_STORAGE_KEY_NAME, userName);
+    paintGreeting(userName);
 }
 
+function paintGreeting(name){
+    greeting.classList.remove(HIDDEN_CLASS);
+    greeting.innerText = `Hello ${name}`;
+}
 
+function activeLoginButton(){
+    if(loginInput.value === ''){
+        loginButton.disabled = true;
+    } else {
+        loginButton.disabled = false;
+    }
+}
 
-loginForm.addEventListener('submit', onLoginSubmit)
-loginButton.addEventListener('click', onLoginSubmit)
+const localStorageKey = localStorage.getItem(LOCAL_STORAGE_KEY_NAME);
+if(localStorageKey === null){
+    loginForm.classList.remove(HIDDEN_CLASS);
+} else {
+    paintGreeting(localStorageKey)
+}
+
+loginForm.addEventListener('submit', onLoginSubmit);
+loginButton.addEventListener('click', onLoginSubmit);
+loginInput.addEventListener('keyup', activeLoginButton);
