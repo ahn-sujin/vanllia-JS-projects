7aafb6db04da240e759ff050d6f335f74cd1701d
asj <asj9674@gmail.com>
2022-04-25 14:23:17 +0900

    todolist 입력 및 localstorage저장

---
+++ js/todo.js
@@ -0,0 +1,54 @@
+const toDoForm = document.querySelector('#todo-form');
+const toDoInput = toDoForm.querySelector('input');
+const toDoButton = toDoForm.querySelector('.todo-btn');
+const toDoList = document.querySelector('#todo-list');
+
+const TODOS_KEY = '할 일';
+
+let toDos = [];
+
+function saveToDo(){
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function checkToDo(event){
+    const checkToDoList = event.target.parentElement;
+    checkToDoList.classList.toggle('on');
+}
+
+function deleteToDo(event){
+    const deleteToDoList = event.target.parentElement;
+    deleteToDoList.remove();
+}
+
+function paintToDo(newToDo){
+    const li =  document.createElement('li');
+    const span = document.createElement('span');
+    const buttonCheck = document.createElement('button');
+    const buttonDelete = document.createElement('button');
+
+    toDoList.appendChild(li);
+    li.appendChild(span);
+    li.appendChild(buttonCheck);
+    li.appendChild(buttonDelete);
+
+    span.innerText = newToDo;
+
+    buttonCheck.classList.add('check');
+    buttonDelete.classList.add('delete');
+
+    buttonCheck.addEventListener('click', checkToDo);
+    buttonDelete.addEventListener('click', deleteToDo);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = '';
+    toDos.push(newToDo);
+    paintToDo(newToDo);
+    saveToDo();
+}
+toDoForm.addEventListener('submit', handleToDoSubmit);
+toDoButton.addEventListener('click', handleToDoSubmit);
+
