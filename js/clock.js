0aebc72d69fd0970bda092f54ce531ff51fc5984
asj <asj9674@gmail.com>
2022-04-27 17:21:43 +0900

    오늘 날짜, 요일 구현 복습

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,30 @@
 const clock = document.querySelector('#clock');
+const day = document.querySelector('#day');
+const date = document.querySelector('#date');
+
+const DATE = new Date();
 
 function getClock(){
-    const date = new Date();
-    const hours = String(date.getHours()).padStart(2,'0');
-    const minutes = String(date.getMinutes()).padStart(2,'0');
-    const seconds = String(date.getSeconds()).padStart(2,'0');;
+    const hours = String(DATE.getHours()).padStart(2,'0');
+    const minutes = String(DATE.getMinutes()).padStart(2,'0');
+    const seconds = String(DATE.getSeconds()).padStart(2,'0');;
 
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
+function getDayOfTheWeek(){
+    const weekNumber = DATE.getDay();
+    const WeekName = ['일요일','월요일','화요일','수요일','목요일','금요일','토요일']
+    
+    day.innerText = `${WeekName[weekNumber]}`;
+}
+
+function getDate(){
+    const todayDate = String(DATE.getDate()).padStart(2,'0');
+    date.innerText = `${todayDate}`;
+}
+
 getClock();
 setInterval(getClock, 1000);
+getDayOfTheWeek();
+getDate();
