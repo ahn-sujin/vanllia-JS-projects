a4f0e980dcec24738d446da10fa3594be60a1e7b
asj <asj9674@gmail.com>
2022-04-27 14:25:17 +0900

    시계 구현 복습

--- js/clock.js
+++ js/clock.js
@@ -1,33 +1,13 @@
 const clock = document.querySelector('#clock');
-const day = document.querySelector('#day');
-const date = document.querySelector('#date');
-
 
 function getClock(){
-    const DATE = new Date();
-    const hours = String(DATE.getHours()).padStart(2,0);
-    const minutes = String(DATE.getMinutes()).padStart(2,0);
-    const seconds = String(DATE.getSeconds()).padStart(2,0);
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,'0');
+    const minutes = String(date.getMinutes()).padStart(2,'0');
+    const seconds = String(date.getSeconds()).padStart(2,'0');;
 
-    clock.innerText = `${hours}:${minutes}:${seconds}`
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
-function getDayOfTheWeek(){
-    const DATE = new Date();
-    const weekNumber = DATE.getDay();
-    const weekName = ['일요일','월요일','화요일','수요일','목요일','금요일','토요일']
-    day.innerText = `${weekName[weekNumber]}`;
-}
-
-function getDate(){
-    const DATE = new Date();
-    const todayDate = String(DATE.getDate()).padStart(2,0);
-    date.innerText = `${todayDate}`
-}
-
-getClock(); 
-setInterval(getClock,1000);
-getDayOfTheWeek();
-getDate();
-
-
+getClock();
+setInterval(getClock, 1000);
