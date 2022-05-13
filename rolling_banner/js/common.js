f3ce4380a0a1c7cadab5f1934136ddb780070572
asj <asj9674@gmail.com>
2022-05-13 17:50:13 +0900

    무한롤링배너 바닐라자바스크립트로 구현

--- rolling_banner/js/common.js
+++ rolling_banner/js/common.js
@@ -1,30 +1,24 @@
-$(document).ready(function(){
+const listBox = document.querySelector('.list-box');
+const list = listBox.querySelector('.list');
+const firstList = document.querySelector('.list:first-child');
+const listWidth = list.offsetWidth;
+const listLength = list.length;
+const totalWidth = listWidth * listLength;
 
-	var wid = $('.list-box .list').outerWidth();
-	var num = $('.list-box .list').length;
-	var totalWid = wid*num;
-	$('.list-box').width(totalWid); //()엔에 값이 없으면 선택자의 넓이 구해주고, 값이 있으면 ()안에 값으로 변경
-	var mleft = 0;
+let mleft = 0;
 
-	var timer = setInterval(move, 20);
-
-	$('.container').on('mouseenter',function(){
-		clearInterval(timer);//
-	});
-	 
-	 $('.container').on('mouseleave',function(){
-		timer = setInterval(move, 20);
-	 });
+function move(){
+    mleft -= 2;
+    if(mleft < -listWidth){
+        listBox.appendChild(firstList);
+        mleft = 0;
+    }
 
 
-	function move(){
-		mleft -= 2;
-		if (mleft < -wid) {
-			$('.list-box .list').first().appendTo('.list-box');
-			mleft = 0;
-		}
-		$('.list-box').css({'left' : mleft});
-	}
+}
 
-	
-});
+
+function RollingBanner(){
+    let timer = setInterval()
+
+}
