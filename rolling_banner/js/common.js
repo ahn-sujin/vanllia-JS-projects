dcf1b8e9113a193f4ed5c877d975cf004fb64791
asj <asj9674@gmail.com>
2022-05-17 11:05:40 +0900

    롤링배너 스크립트 구현 완료

--- rolling_banner/js/common.js
+++ rolling_banner/js/common.js
@@ -1,25 +1,30 @@
-const listBox = document.querySelector('.list-box');
-const list = listBox.querySelector('.list');
-const firstList = document.querySelector('.list:first-child');
-const listWidth = list.offsetWidth;
-const listLength = list.length;
-const totalWidth = listWidth * listLength;
+let originalID;
+let cloneID;
 
-let mleft = 0;
+const roller = document.querySelector('.roller');
+roller.id = 'roller1';
 
-function move(){
-    mleft -= 2;
-    if(mleft < -listWidth){
-        listBox.appendChild(firstList);
-        mleft = 0;
+const clone = roller.cloneNode(true);
+clone.id = 'roller2';
+document.querySelector('.wrap').appendChild(clone);
+
+document.querySelector('#roller1').style.left = '0px';
+document.querySelector('#roller2').style.left = document.querySelector('.roller ul').offsetWidth + 'px';
+
+roller.classList.add('original');
+clone.classList.add('clone');
+
+let rollerWidth = document.querySelector('.roller ul').offsetWidth;//회전 배너 너비값
+let betweenDistance = 1;//이동 크기 - 정수여야 함
+originalID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller1'));
+cloneID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller2'));
+
+function betweenRollCallback(d, roller) {
+    let left = parseInt(roller.style.left);
+    roller.style.left = (left - d) + 'px';//이동
+    //조건부 위치 리셋
+    if (rollerWidth + (left - d) <= 0) {
+        roller.style.left = rollerWidth + 'px';
     }
-    
-
-
 }
 
-
-function RollingBanner(){
-    let timer = setInterval()
-
-}
