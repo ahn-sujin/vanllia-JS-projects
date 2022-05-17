2901378bac75f1387e9412fbd71b90f3a3933b2e
asj <asj9674@gmail.com>
2022-05-17 13:36:31 +0900

    롤링배너 리팩토링

--- rolling_banner/js/common.js
+++ rolling_banner/js/common.js
@@ -1,36 +1,25 @@
-let originalID;
-let cloneID;
 
+const rollerWrap = document.querySelector('.wrap');
 const roller = document.querySelector('.roller');
 roller.id = 'roller1';
-
 const clone = roller.cloneNode(true);
 clone.id = 'roller2';
-document.querySelector('.wrap').appendChild(clone);
+rollerWrap.appendChild(clone);
+const originalRoller = document.querySelector('#roller1')
+const cloneRoller = document.querySelector('#roller2')
+const rollerWidth = document.querySelector('.roller ul').offsetWidth;
+originalRoller.style.left = '0px';
+cloneRoller.style.left = rollerWidth + 'px';
 
-document.querySelector('#roller1').style.left = '0px';
-document.querySelector('#roller2').style.left = document.querySelector('.roller ul').offsetWidth + 'px';
+let BETWEEN_DISTANCE = 1;
 
-roller.classList.add('original');
-clone.classList.add('clone');
-
-let rollerWidth = document.querySelector('.roller ul').offsetWidth;//회전 배너 너비값
-let betweenDistance = 1;//이동 크기 - 정수여야 함
-originalID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller1'));
-cloneID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller2'));
-
-function betweenRollCallback(d, roller) {
-    let left = parseInt(roller.style.left);
-    roller.style.left = (left - d) + 'px';//이동
-    //조건부 위치 리셋
-    if (rollerWidth + (left - d) <= 0) {
-        roller.style.left = rollerWidth + 'px';
+function betweenRollCallback(distance, item) {
+    const location = parseInt(item.style.left);
+    item.style.left = (location - distance) + 'px';
+    if (rollerWidth + (location - distance) <= 0) {
+        item.style.left = rollerWidth + 'px';
     }
 }
 
-function stop(){
-    clearInterval(originalID);
-    clearInterval(cloneID);
-}
-
-roller.addEventListener('mouseover', stop);
+setInterval(betweenRollCallback, 10, BETWEEN_DISTANCE, originalRoller);
+setInterval(betweenRollCallback, 10, BETWEEN_DISTANCE, cloneRoller);
